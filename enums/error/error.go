517f8b5096cf725870789c9c0d12fe3c2e134cbb
chenbiao <chenbiao@reta-inc.com>
2023-09-15 10:39:46 +0800

    init

--- enums/error/error.go
+++ enums/error/error.go
@@ -24,23 +24,23 @@ func SimpleDgError(msg string) *DgError {
 }
 
 var (
-	SYSTEM_ERROR = &DgError{5001, "System error"}
-	SYSTEM_BUSY  = &DgError{5002, "System invalid"}
+	SYSTEM_ERROR = &DgError{5001, "系统错误"}
+	SYSTEM_BUSY  = &DgError{5002, "系统繁忙"}
 
-	ARGUMENT_NOT_VALID     = &DgError{4001, "Invalid arguments"}
-	INVALID_TOKEN          = &DgError{4002, "Invalid token"}
-	UPLOAD_FILE_SIZE_LIMIT = &DgError{4003, "File size overflow"}
-	DUPLICATE_PRIMARY_KEY  = &DgError{4004, "Multiple record"}
+	ARGUMENT_NOT_VALID     = &DgError{4001, "无效参数"}
+	INVALID_TOKEN          = &DgError{4002, "无效token"}
+	UPLOAD_FILE_SIZE_LIMIT = &DgError{4003, "文件过大"}
+	DUPLICATE_PRIMARY_KEY  = &DgError{4004, "重复主键"}
 
-	LOGIN_ERROR       = &DgError{4005, "Login error"}
-	NOT_LOGIN_IN      = &DgError{4006, "User not login"}
-	USER_NOT_EXISTS   = &DgError{4007, "User not exist"}
-	WRONG_PASSWORD    = &DgError{4008, "Wrong password"}
-	NO_PERMISSION     = &DgError{4009, "No permission"}
-	ILLEGAL_OPERATION = &DgError{4010, "Illegal operation"}
+	LOGIN_ERROR       = &DgError{4005, "登录错误"}
+	NOT_LOGIN_IN      = &DgError{4006, "用户未登录"}
+	USER_NOT_EXISTS   = &DgError{4007, "用户不存在"}
+	WRONG_PASSWORD    = &DgError{4008, "密码错误"}
+	NO_PERMISSION     = &DgError{4009, "无权限"}
+	ILLEGAL_OPERATION = &DgError{4010, "非法操作"}
 
-	RECORD_NOT_EXISTS     = &DgError{4014, "Record not exist"}
-	DISABLED_USER         = &DgError{4015, "Disabled user"}
-	EMAIL_REGISTERED      = &DgError{4016, "Email has been registered"}
+	RECORD_NOT_EXISTS     = &DgError{4014, "记录不存在"}
+	DISABLED_USER         = &DgError{4015, "用户已被禁用"}
+	EMAIL_REGISTERED      = &DgError{4016, "邮箱已被注册"}
 	CUSTOMER_HAS_NO_GROUP = &DgError{4031, "该企业暂无国家对应团队"}
 )

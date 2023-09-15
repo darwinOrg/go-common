69450375e3eae3d71ee133df58b887f814832b5a
chenbiao <chenbiao@reta-inc.com>
2023-09-15 16:52:03 +0800

    init

--- model/pair.go
+++ model/pair.go
@@ -1,8 +1,28 @@
-package pair
+package model
+
+import "encoding/json"
+
+type IntCodeNamePair struct {
+	Code int    `json:"code"`
+	Name string `json:"name"`
+}
+
+type IdNamePair struct {
+	Id   int64  `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
 
 type StringCodeNamePair struct {
 	Code string `json:"code"`
 	Name string `json:"name"`
+}
+
+func (pair *IdNamePair) String() string {
+	js, err := json.Marshal(pair)
+	if err != nil {
+		return err.Error()
+	}
+	return string(js)
 }
 
 func FindStringPairName(pairs []*StringCodeNamePair, code string, defaultVal string) string {
@@ -17,11 +37,6 @@ func FindStringPairName(pairs []*StringCodeNamePair, code string, defaultVal str
 	}
 
 	return defaultVal
-}
-
-type IntCodeNamePair struct {
-	Code int    `json:"code"`
-	Name string `json:"name"`
 }
 
 func FindIntPairName(pairs []*IntCodeNamePair, code int, defaultVal string) string {

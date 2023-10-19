39a6ad086e9ef1c29b14ab62d950bd137a85d54a
chenbiao <149027310@qq.com>
2023-10-19 09:49:04 +0800

    req

---
+++ model/req.go
@@ -0,0 +1,17 @@
+package model
+
+type IdReq struct {
+	Id int64 `json:"id" binding:"required,gte=1"`
+}
+
+type IdListReq struct {
+	IdList []int64 `json:"idList" binding:"required,lt=0"`
+}
+
+type StringReq struct {
+	Value string `json:"value" binding:"required"`
+}
+
+type StringListReq struct {
+	Values string `json:"values" binding:"required,lt=0"`
+}

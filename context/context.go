b0a47b0f75061ba435cf4e424fb326feb436217b
chenbiao <149027310@qq.com>
2023-11-10 09:41:02 +0800

    DgContext

--- context/context.go
+++ context/context.go
@@ -1,23 +1,23 @@
 package dgctx
 
 type DgContext struct {
-	TraceId    string
-	UserId     int64
-	OpId       int64
-	RunAs      int64
-	Roles      string
-	BizTypes   int
-	GroupId    int64
-	Platform   string
-	UserAgent  string
-	Lang       string
-	GoId       uint64
-	Token      string
-	ShareToken string
-	RemoteIp   string
-	CompanyId  int64
-	Product    int
-	Extra      map[string]any
+	TraceId    string         `json:"traceId,omitempty"`
+	UserId     int64          `json:"userId,omitempty"`
+	OpId       int64          `json:"opId,omitempty"`
+	RunAs      int64          `json:"runAs,omitempty"`
+	Roles      string         `json:"roles,omitempty"`
+	BizTypes   int            `json:"bizTypes,omitempty"`
+	GroupId    int64          `json:"groupId,omitempty"`
+	Platform   string         `json:"platform,omitempty"`
+	UserAgent  string         `json:"userAgent,omitempty"`
+	Lang       string         `json:"lang,omitempty"`
+	GoId       uint64         `json:"goId,omitempty"`
+	Token      string         `json:"token,omitempty"`
+	ShareToken string         `json:"shareToken,omitempty"`
+	RemoteIp   string         `json:"remoteIp,omitempty"`
+	CompanyId  int64          `json:"companyId,omitempty"`
+	Product    int            `json:"product,omitempty"`
+	Extra      map[string]any `json:"extra,omitempty"`
 }
 
 func (ctx *DgContext) SetExtraKeyValue(key string, val any) {

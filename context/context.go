da2d6e69afd2e7e5d001bfa2b70a26cc1a06b55d
chenbiao <chenbiao@reta-inc.com>
2023-12-14 09:58:23 +0800

    init

--- context/context.go
+++ context/context.go
@@ -3,30 +3,46 @@ package dgctx
 import "sync"
 
 type DgContext struct {
-	TraceId    string   `json:"traceId,omitempty"`
-	UserId     int64    `json:"userId,omitempty"`
-	OpId       int64    `json:"opId,omitempty"`
-	RunAs      int64    `json:"runAs,omitempty"`
-	Roles      string   `json:"roles,omitempty"`
-	BizTypes   int      `json:"bizTypes,omitempty"`
-	GroupId    int64    `json:"groupId,omitempty"`
-	Platform   string   `json:"platform,omitempty"`
-	UserAgent  string   `json:"userAgent,omitempty"`
-	Lang       string   `json:"lang,omitempty"`
-	GoId       uint64   `json:"goId,omitempty"`
-	Token      string   `json:"token,omitempty"`
-	ShareToken string   `json:"shareToken,omitempty"`
-	RemoteIp   string   `json:"remoteIp,omitempty"`
-	CompanyId  int64    `json:"companyId,omitempty"`
-	Product    int      `json:"product,omitempty"`
-	Extra      sync.Map `json:"-"`
+	TraceId     string `json:"traceId,omitempty"`
+	UserId      int64  `json:"userId,omitempty"`
+	OpId        int64  `json:"opId,omitempty"`
+	RunAs       int64  `json:"runAs,omitempty"`
+	Roles       string `json:"roles,omitempty"`
+	BizTypes    int    `json:"bizTypes,omitempty"`
+	GroupId     int64  `json:"groupId,omitempty"`
+	Platform    string `json:"platform,omitempty"`
+	UserAgent   string `json:"userAgent,omitempty"`
+	Lang        string `json:"lang,omitempty"`
+	GoId        uint64 `json:"goId,omitempty"`
+	Token       string `json:"token,omitempty"`
+	ShareToken  string `json:"shareToken,omitempty"`
+	RemoteIp    string `json:"remoteIp,omitempty"`
+	CompanyId   int64  `json:"companyId,omitempty"`
+	Product     int    `json:"product,omitempty"`
+	safeExtra   sync.Map
+	unsafeExtra map[string]any
 }
 
 func (ctx *DgContext) SetExtraKeyValue(key string, val any) {
-	ctx.Extra.Store(key, val)
+	ctx.safeExtra.Store(key, val)
 }
 
 func (ctx *DgContext) GetExtraValue(key string) any {
-	val, _ := ctx.Extra.Load(key)
+	val, _ := ctx.safeExtra.Load(key)
 	return val
+}
+
+func (ctx *DgContext) SetUnsafeExtraKeyValue(key string, val any) {
+	if ctx.unsafeExtra == nil {
+		ctx.unsafeExtra = make(map[string]any)
+	}
+	ctx.unsafeExtra[key] = val
+}
+
+func (ctx *DgContext) GetUnsafeExtraValue(key string) any {
+	if ctx.unsafeExtra == nil {
+		return nil
+	}
+
+	return ctx.unsafeExtra[key]
 }

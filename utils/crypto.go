bd97aafdc995e5b8767722c84f71f055e34469b6
chenbiao <149027310@qq.com>
2024-01-29 14:57:47 +0800

    Sha256Base64Encode

--- utils/crypto.go
+++ utils/crypto.go
@@ -3,11 +3,20 @@ package utils
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/base64"
 )
 
 func Sha1Base64Encode(key string, content string) string {
 	hash := hmac.New(sha1.New, []byte(key))
+	hash.Write([]byte(content))
+	bytes := hash.Sum(nil)
+
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+func Sha256Base64Encode(key string, content string) string {
+	hash := hmac.New(sha256.New, []byte(key))
 	hash.Write([]byte(content))
 	bytes := hash.Sum(nil)
 

bedede876efc69315c8bffd50cc28650559a3c08
chenbiao <149027310@qq.com>
2024-04-10 12:44:25 +0800

    Trans2NewMap

--- collection/map.go
+++ collection/map.go
@@ -7,8 +7,36 @@ func Trans2NewMap[T comparable, K any, V any](mp map[T]K, mapFunc Function[K, V]
 
 	newMap := make(map[T]V, len(mp))
 
-	for k, v := range mp {
-		newMap[k] = mapFunc(v)
+	for t, k := range mp {
+		newMap[t] = mapFunc(k)
+	}
+
+	return newMap
+}
+
+func Trans2NewListMap[T comparable, K any, V any](mp map[T][]K, mapFunc Function[K, V]) map[T][]V {
+	if len(mp) == 0 {
+		return map[T][]V{}
+	}
+
+	newMap := make(map[T][]V, len(mp))
+
+	for t, keys := range mp {
+		newMap[t] = MapToList(keys, mapFunc)
+	}
+
+	return newMap
+}
+
+func Trans2NewSetMap[T comparable, K any, V comparable](mp map[T][]K, mapFunc Function[K, V]) map[T][]V {
+	if len(mp) == 0 {
+		return map[T][]V{}
+	}
+
+	newMap := make(map[T][]V, len(mp))
+
+	for t, keys := range mp {
+		newMap[t] = MapToSet(keys, mapFunc)
 	}
 
 	return newMap

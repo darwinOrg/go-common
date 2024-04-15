98dd5d44c096c88b05efbc1fba24cc2bb92b7026
chenbiao <149027310@qq.com>
2024-04-15 17:21:07 +0800

    Sha1HexEncode、Sha256HexEncode

--- utils/crypto.go
+++ utils/crypto.go
@@ -24,8 +24,18 @@ func Sha256Base64Encode(key string, content string) string {
 	return base64.StdEncoding.EncodeToString(bytes)
 }
 
-func Sha1HmacHexEncode(key string, content string) string {
-	mac := hmac.New(sha1.New, []byte(key))
-	mac.Write([]byte(content))
-	return hex.EncodeToString(mac.Sum(nil))
+func Sha1HexEncode(key string, content string) string {
+	hash := hmac.New(sha1.New, []byte(key))
+	hash.Write([]byte(content))
+	bytes := hash.Sum(nil)
+
+	return hex.EncodeToString(bytes)
+}
+
+func Sha256HexEncode(key string, content string) string {
+	hash := hmac.New(sha256.New, []byte(key))
+	hash.Write([]byte(content))
+	bytes := hash.Sum(nil)
+
+	return hex.EncodeToString(bytes)
 }

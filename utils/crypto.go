65ae5aa54734c92ce66fa462442bb2dc0cbf7ce5
chenbiao <149027310@qq.com>
2024-04-15 17:18:59 +0800

    Sha1HmacHexEncode

--- utils/crypto.go
+++ utils/crypto.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 )
 
 func Sha1Base64Encode(key string, content string) string {
@@ -21,4 +22,10 @@ func Sha256Base64Encode(key string, content string) string {
 	bytes := hash.Sum(nil)
 
 	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+func Sha1HmacHexEncode(key string, content string) string {
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(content))
+	return hex.EncodeToString(mac.Sum(nil))
 }

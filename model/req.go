10815ba1f121f019ee350910f83283977c20d609
chenbiao <149027310@qq.com>
2024-08-26 17:50:54 +0800

    refactor

--- model/req.go
+++ model/req.go
@@ -1,9 +1,9 @@
 package model
 
 type IdReq struct {
-	Id int64 `json:"id" form:"id" binding:"required,gt=0" remark:"id"`
+	Id int64 `json:"id" form:"id" binding:"required" remark:"id"`
 }
 
-type IdListReq struct {
-	IdList []int64 `json:"idList" form:"idList" binding:"required,gt=0" remark:"id列表"`
+type IdsReq struct {
+	Ids []int64 `json:"ids" form:"ids" binding:"required" remark:"id列表"`
 }

4e0a3524142fcaa304bedfb56a34c46559118954
chenbiao <149027310@qq.com>
2024-10-30 15:52:16 +0800

    up

--- utils/exec.go
+++ utils/exec.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"log"
@@ -9,50 +8,44 @@ import (
 	"time"
 )
 
-func ExecCommand(command string, arg ...string) error {
+func ExecCommand(command string, arg ...string) (string, error) {
 	start := time.Now().UnixMilli()
 	cmd := exec.Command(command, arg...)
-	var out, stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
 
-	err := cmd.Run()
+	output, err := cmd.CombinedOutput()
 	cost := time.Now().UnixMilli() - start
 	if err != nil {
 		if cmd.ProcessState.ExitCode() == 200 {
 			log.Printf("execute command %s ok, cost %dms", command, cost)
-			return nil
+			return string(output), nil
 		}
-		log.Printf("execute command %s error: %v\nstdout: %s\nstderr: %s", command, err, out.String(), stderr.String())
-		return err
+		log.Printf("execute command %s error: %v\noutput: %s", command, err, string(output))
+		return "", err
 	}
 
-	return nil
+	return string(output), nil
 }
 
-func ExecCommandWithTimeout(command string, timeout time.Duration, arg ...string) error {
+func ExecCommandWithTimeout(command string, timeout time.Duration, arg ...string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	start := time.Now().UnixMilli()
 	cmd := exec.CommandContext(ctx, command, arg...)
-	var out, stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
 
-	err := cmd.Run()
+	output, err := cmd.CombinedOutput()
 	cost := time.Now().UnixMilli() - start
 	if err != nil {
 		if cmd.ProcessState.ExitCode() == 200 {
 			log.Printf("execute command %s ok, cost %dms", command, cost)
-			return nil
+			return string(output), nil
 		}
 		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 			log.Printf("execute command %s timeout, cost %dms", command, cost)
-			return context.DeadlineExceeded
+			return string(output), context.DeadlineExceeded
 		}
-		log.Printf("execute command %s error: %v\nstdout: %s\nstderr: %s", command, err, out.String(), stderr.String())
-		return err
+		log.Printf("execute command %s error: %v\noutput: %s", command, err, string(output))
+		return "", err
 	}
 
-	return nil
+	return string(output), nil
 }

4b4b334e2f1fd76a621e93f01ee3e12e5909c53f
chenbiao <149027310@qq.com>
2025-03-17 14:17:47 +0800

    FormatToChineseDuration

--- utils/time.go
+++ utils/time.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 func ParseFromOneDateFormatToAnother(str, fromFormat, toFormat string) (string, error) {
 	fromTime, err := time.Parse(fromFormat, str)
@@ -15,4 +18,13 @@ func MustParseFromOneDateFormatToAnother(str, fromFormat, toFormat string) strin
 	fromTime, _ := ParseFromOneDateFormatToAnother(str, fromFormat, toFormat)
 
 	return fromTime
+}
+
+func FormatToChineseDuration(d time.Duration) string {
+	str := d.String()
+	str = strings.ReplaceAll(str, "h", "小时")
+	str = strings.ReplaceAll(str, "m", "分")
+	str = strings.ReplaceAll(str, "s", "秒")
+
+	return str
 }

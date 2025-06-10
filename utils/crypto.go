0364a4b8deaac6fd360b2a119f241972fd3df7bd
chenbiao <149027310@qq.com>
2025-06-10 16:00:35 +0800

    AesCBCEncrypt & AesCBCDecrypt

--- utils/crypto.go
+++ utils/crypto.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/md5"
 	"crypto/rand"
@@ -9,10 +11,94 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"math/big"
 	"os"
 )
+
+func AesCBCEncrypt(key, content string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	contentBytes := []byte(content)
+	blockSize := block.BlockSize()
+
+	// Apply PKCS7 padding
+	paddedContent := PKCS7Padding(contentBytes, blockSize)
+
+	ciphertext := make([]byte, aes.BlockSize+len(paddedContent))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], paddedContent)
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func AesCBCDecrypt(key, content string) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(content)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", fmt.Errorf("ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	if len(ciphertext)%block.BlockSize() != 0 {
+		return "", fmt.Errorf("ciphertext length not aligned to block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	plaintextPadded := make([]byte, len(ciphertext))
+	mode.CryptBlocks(plaintextPadded, ciphertext)
+
+	// Remove PKCS7 padding
+	plaintext, err := PKCS7UnPadding(plaintextPadded)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func PKCS7Padding(data []byte, blockSize int) []byte {
+	padding := blockSize - (len(data) % blockSize)
+	padByte := byte(padding)
+	paddingBytes := make([]byte, padding)
+	for i := range paddingBytes {
+		paddingBytes[i] = padByte
+	}
+	return append(data, paddingBytes...)
+}
+
+func PKCS7UnPadding(data []byte) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, fmt.Errorf("data is empty")
+	}
+
+	padding := int(data[length-1])
+	if padding > length || padding > aes.BlockSize {
+		return nil, fmt.Errorf("invalid padding")
+	}
+
+	return data[:(length - padding)], nil
+}
 
 func Sha1Base64Encode(key string, content string) string {
 	hash := hmac.New(sha1.New, []byte(key))

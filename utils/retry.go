6d7239f8a576073ef6569842b7f276ca828a94e9
chenbiao <149027310@qq.com>
2025-07-29 17:07:44 +0800

    up

--- utils/retry.go
+++ utils/retry.go
@@ -2,12 +2,13 @@ package utils
 
 import "time"
 
-func Retry(maxTimes int, sleepInterval time.Duration, fn func() error) {
+func Retry(maxTimes int, sleepInterval time.Duration, fn func() error) (err error) {
 	for i := 0; i < maxTimes; i++ {
-		if err := fn(); err == nil {
+		if err = fn(); err == nil {
 			return
 		}
 
 		time.Sleep(sleepInterval)
 	}
+	return
 }
--- utils/time.go
+++ utils/time.go
@@ -28,3 +28,71 @@ func FormatToChineseDuration(d time.Duration) string {
 
 	return str
 }
+
+func FormatToChineseYearMonthDay(t time.Time) string {
+	return t.Format("2006年1月2日")
+}
+
+func FormatToChineseMonthDay(t time.Time) string {
+	return t.Format("1月2日")
+}
+
+func CurrentDate() time.Time {
+	return StartOfDay(time.Now())
+}
+
+func StartOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func EndOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, t.Location())
+}
+
+func NextDay(t time.Time) time.Time {
+	return NextDays(t, 1)
+}
+
+func NextDays(t time.Time, days int) time.Time {
+	return t.AddDate(0, 0, days)
+}
+
+func PreDay(t time.Time) time.Time {
+	return PreDays(t, 1)
+}
+
+func PreDays(t time.Time, days int) time.Time {
+	return t.AddDate(0, 0, -days)
+}
+
+func NextMonth(t time.Time) time.Time {
+	return NextMonths(t, 1)
+}
+
+func NextMonths(t time.Time, months int) time.Time {
+	return t.AddDate(0, months, 0)
+}
+
+func PreMonth(t time.Time) time.Time {
+	return PreMonths(t, 1)
+}
+
+func PreMonths(t time.Time, months int) time.Time {
+	return t.AddDate(0, -months, 0)
+}
+
+func NextYear(t time.Time) time.Time {
+	return NextYears(t, 1)
+}
+
+func NextYears(t time.Time, years int) time.Time {
+	return t.AddDate(years, 0, 0)
+}
+
+func PreYear(t time.Time) time.Time {
+	return PreYears(t, 1)
+}
+
+func PreYears(t time.Time, years int) time.Time {
+	return t.AddDate(-years, 0, 0)
+}

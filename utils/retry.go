b4513d206f9bc43d9d678a8fe6cb86df7eaabf13
陈彪 <chenbiao@globalpand.cn>
2026-03-24 11:16:20 +0800

    RetryWithResult

--- utils/retry.go
+++ utils/retry.go
@@ -13,6 +13,17 @@ func Retry(maxTimes int, sleepInterval time.Duration, fn func() error) (err erro
 	return
 }
 
+func RetryWithResult[T any](maxTimes int, sleepInterval time.Duration, fn func() (T, error)) (t T, err error) {
+	for i := 0; i < maxTimes; i++ {
+		if t, err = fn(); err == nil {
+			return t, nil
+		}
+
+		time.Sleep(sleepInterval)
+	}
+	return
+}
+
 func RetryWithIndex(maxTimes int, sleepInterval time.Duration, fn func(i int) error) (err error) {
 	for i := 0; i < maxTimes; i++ {
 		if err = fn(i); err == nil {
